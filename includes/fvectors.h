7a9297fc322948406d300cd81c9f2b9a9ac09769
ggjulio <juligonz@student.42.fr>
2020-02-06 17:09:43 +0100

    norming

--- includes/fvectors.h
+++ includes/fvectors.h
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 15:12:12 by juligonz          #+#    #+#             */
-/*   Updated: 2020/02/01 12:00:09 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/06 17:07:12 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct	s_fvector
 
 t_fvector		create_fvector(double x, double y);
 t_fvector		perp_clock_fvec(t_fvector fvector);
-t_fvector   	perp_cntclock_fvec(t_fvector fvector);
+t_fvector		perp_cntclock_fvec(t_fvector fvector);
 t_fvector		multiply_fvec_by_dbl(t_fvector fvector, double n);
 
 #endif
