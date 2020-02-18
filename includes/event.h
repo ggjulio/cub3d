e2c3775178c52c69eb52684b149caac6d67a5cf5
ggjulio <juligonz@student.42.fr>
2020-02-18 15:53:25 +0100

    .

--- includes/event.h
+++ includes/event.h
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 14:50:30 by juligonz          #+#    #+#             */
-/*   Updated: 2020/02/17 12:47:08 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/18 15:29:47 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int		is_key_press(int key);
 int		is_key_release(int key);
 int		is_focus_out(void);
 int		is_focus_in(void);
+int		is_configure_notify(void);
 int		is_mouse_moved(int key);
 int		close_program(int key);
 
--- srcs/events/events.c
+++ srcs/events/events.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 16:44:33 by juligonz          #+#    #+#             */
-/*   Updated: 2020/02/17 17:56:00 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/18 15:51:55 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,22 @@ int		is_focus_out(void)
 				   g_app.res.y / 2.1,
 				   color_str.c, "Resume Game");
     mlx_loop_hook(g_app.mlx_ptr, NULL, NULL);
+	return (0);
+}
+
+int		is_configure_notify(void)
+{
+	t_vector size;
+
+	mlx_window_get_size(g_app.win_ptr, &(size.x), &(size.y));
+	ft_printf("Configure Notify %d | %d \n", size.x, size.y);
+    if (g_app.img_ptr != NULL)
+        mlx_destroy_image(g_app.mlx_ptr, g_app.img_ptr);
+	g_app.res.x = size.x;
+	g_app.res.y = size.y;
+	g_app.img_ptr = mlx_new_image(g_app.mlx_ptr, size.x, size.y);
+	g_app.pixels = mlx_get_data_addr(g_app.img_ptr, &(g_app.bits_per_pixel),
+									 &(g_app.size_line), &(g_app.endian));
 	return (0);
 }
 
