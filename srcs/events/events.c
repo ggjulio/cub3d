98881adf03d8b939cdc69630d861aefa85fcb75d
ggjulio <juligonz@student.42.fr>
2020-02-20 17:06:35 +0100

    .

--- srcs/events/events.c
+++ srcs/events/events.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/22 16:44:33 by juligonz          #+#    #+#             */
-/*   Updated: 2020/02/18 17:42:34 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/02/20 17:06:21 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,16 @@ int		is_focus_out(void)
 {
 	t_color color_str;
 
-	color_str = create_color(255,255,255,255);
+	color_str = create_color(255, 255, 255, 255);
 	clear_application(
-		create_color(0,0,0,100));
+		create_color(0, 0, 0, 100));
 	render_application();
-	mlx_string_put(g_app.mlx_ptr, g_app.win_ptr, 
-				   g_app.res.x / 2.3, 
-				   g_app.res.y / 2.1,
-				   color_str.c, "Resume Game");
-    mlx_loop_hook(g_app.mlx_ptr, NULL, NULL);
+	mlx_string_put(g_app.mlx_ptr, g_app.win_ptr,
+				g_app.res.x / 2.3,
+				g_app.res.y / 2.1,
+				color_str.c, "Resume Game");
+	mlx_window_set_title(g_app.win_ptr, "Super Cub3D Paused :)");
+	mlx_loop_hook(g_app.mlx_ptr, NULL, NULL);
 	return (0);
 }
 
@@ -37,14 +38,14 @@ int		is_configure_notify(void)
 	ratio = fabs((double)size.x / (double)size.y);
 	g_game.cam.plane = (t_fvector){
 		(-0.5 * ratio) * g_game.cam.dir.y,
-		(0.5 * ratio)* g_game.cam.dir.x,
+		(0.5 * ratio) * g_game.cam.dir.x,
 	};
 	return (0);
 }
 
 int		is_focus_in(void)
 {
-    mlx_loop_hook(g_app.mlx_ptr, loop_game, NULL);
+	mlx_loop_hook(g_app.mlx_ptr, loop_game, NULL);
 	return (0);
 }
 
