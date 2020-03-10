fd418d7e5e0888739dd7696d519ebe596548cdaa
ggjulio <juligonz@student.42.fr>
2020-03-10 16:35:47 +0100

    .

--- srcs/sprite/sprite_util.c
+++ srcs/sprite/sprite_util.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 13:17:48 by juligonz          #+#    #+#             */
-/*   Updated: 2020/03/10 12:11:45 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/03/10 16:35:37 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,43 @@
 
 void	draw_sprite(t_raycast *r, int x, t_sprite *sprite)
 {
-	t_vector	s_size;
-	t_vector	draw ;
+	t_vector	sprite_size;
+	t_vector	y_draw ;
 	int			tex_x;
 	float		tex_y;
 	float		step_y;
 	
-	double inv_determinant =
-		1.0 / g_game.cam.dir.y * g_game.cam.plane.x - g_game.cam.dir.x * g_game.cam.plane.y;
+	double inv_determinant = 1.0 / (g_game.cam.plane.x * g_game.cam.dir.y - g_game.cam.dir.x * g_game.cam.plane.y);
 	t_fvector transform = create_fvector(
-inv_determinant * (g_game.cam.dir.y * sprite->pos_rel_cam.x - g_game.cam.dir.x * sprite->pos_rel_cam.y),
-inv_determinant * (-g_game.cam.plane.y * sprite->pos_rel_cam.x - g_game.cam.plane.x * sprite->pos_rel_cam.y)
+		inv_determinant * (g_game.cam.dir.y * sprite->pos_rel_cam.x - g_game.cam.dir.x * sprite->pos_rel_cam.y),
+		inv_determinant * (-g_game.cam.plane.y * sprite->pos_rel_cam.x + g_game.cam.plane.x * sprite->pos_rel_cam.y)
 		);
 	
-	s_size.x = g_app.res.x * 0.5 * (1 + transform.x / transform.y);
-	s_size.y = g_app.res.y / transform.y;
-	
+	sprite_size.x = g_app.res.x * 0.5 * (1 + transform.x / transform.y);
+	sprite_size.y = abs((int)(g_app.res.y / transform.y));
+
 //////
-	draw.x = -s_size.y * 0.5 + g_app.res.y * 0.5;
-	draw.y = s_size.y * 0.5 + g_app.res.y * 0.5;	
-	draw.x =(draw.x < 0 ? 0 : draw.x);
-	draw.y =(draw.y > g_app.res.y ? g_app.res.y : draw.y);
+	y_draw.x = -sprite_size.y * 0.5 + g_app.res.y * 0.5;
+	y_draw.y = sprite_size.y * 0.5 + g_app.res.y * 0.5;
+
+	printf("(%d,%d)\n", y_draw.x, y_draw.y);
+	y_draw.x = (y_draw.x < 0 ? 0 : y_draw.x);
+	y_draw.y = (y_draw.y > g_app.res.y ? g_app.res.y : y_draw.y);
 /////////
 
-	ft_printf("(%d,%d)\n", draw.x, draw.y);
-	tex_x = r->wall_x * sprite->texture->img.size.x;
-	
-	step_y = (float)sprite->texture->img.size.y / (float)r->line_height;
-	tex_y = 
-fabs(draw.x - g_app.res.y * g_game.cam.height +  s_size.y * (1.0 - g_game.cam.height)) * step_y;
-	
-//	d = (y) * 256 - h * 128 + spriteHeight * 128
-//	tex_y = ((d * texHeight) / spriteHeight) / 256;
+	tex_x = (x - (-sprite_size.x / 2 + sprite_size.x)) * sprite->texture->img.size.x / sprite_size.y;
 
-	while (draw.x++ < draw.y)
+	(void)r;
+	step_y = (float)sprite->texture->img.size.y / (float)sprite_size.y;
+	tex_y = 
+		fabs(y_draw.x - g_app.res.y * g_game.cam.height +  sprite_size.y * (1.0 - g_game.cam.height)) * step_y;
+
+	while (y_draw.x++ < y_draw.y)
 	{
 		t_color		texel = create_color(0, 130, 200, 100);
-		
+
 		texel = get_pixel_from_image(sprite->texture->img, tex_x, (int)tex_y);
-		put_pixel(create_vector(x, draw.x), texel);
+		put_pixel(create_vector(x, y_draw.x), texel);
 		tex_y += step_y;
 	}
 	
