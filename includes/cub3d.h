9fda9de21a2534595c5cc0d6a938df4d7fd71ced
ggjulio <juligonz@student.42.fr>
2020-03-12 22:12:02 +0100

    corona

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 18:21:40 by juligonz          #+#    #+#             */
-/*   Updated: 2020/03/12 21:53:17 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/03/12 22:02:43 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@
 # define WIN_MIN_WIDTH 100
 # define WIN_MIN_HEIGHT 100
 
-# define LIFE 4
+# define LIFE 100
 
 # define CAMERA_HEIGHT 0.5
 # define CAMERA_HEIGHT_SQUAT 0.45
--- srcs/move/move_movement.c
+++ srcs/move/move_movement.c
@@ -6,14 +6,33 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/26 13:02:04 by juligonz          #+#    #+#             */
-/*   Updated: 2020/03/08 14:38:23 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/03/12 22:11:38 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	forward(double speed)
+/*
+** Don't judge me.
+*/
+
+static void	check_is_monster(double x, double y)
 {
+	if (2 == map_value(x, y))
+	{
+		g_game.life--;
+		ft_printf("%d\n", g_game.life);
+	}
+	if (g_game.life < 1)
+		ft_printf("dead\n");
+}
+
+void		forward(double speed)
+{
+	check_is_monster(
+(int)(g_game.cam.pos.x + g_game.cam.dir.x * speed), (int)g_game.cam.pos.y);
+	check_is_monster(
+(int)g_game.cam.pos.x, (int)(g_game.cam.pos.y + g_game.cam.dir.y * speed));
 	if (!map_value(
 	(int)(g_game.cam.pos.x + g_game.cam.dir.x * speed), (int)g_game.cam.pos.y))
 		g_game.cam.pos.x += g_game.cam.dir.x * speed;
@@ -22,8 +41,12 @@ void	forward(double speed)
 		g_game.cam.pos.y += g_game.cam.dir.y * speed;
 }
 
-void	backward(double speed)
+void		backward(double speed)
 {
+	check_is_monster(
+(int)(g_game.cam.pos.x - g_game.cam.dir.x * speed), (int)g_game.cam.pos.y);
+	check_is_monster(
+(int)g_game.cam.pos.x, (int)(g_game.cam.pos.y - g_game.cam.dir.y * speed));
 	if (!map_value(
 	(int)(g_game.cam.pos.x - g_game.cam.dir.x * speed), (int)g_game.cam.pos.y))
 		g_game.cam.pos.x -= g_game.cam.dir.x * speed;
@@ -32,8 +55,12 @@ void	backward(double speed)
 		g_game.cam.pos.y -= g_game.cam.dir.y * speed;
 }
 
-void	left(double lat_speed)
+void		left(double lat_speed)
 {
+	check_is_monster((int)(g_game.cam.pos.x - g_game.cam.plane.x * lat_speed),
+(int)g_game.cam.pos.y);
+	check_is_monster((int)g_game.cam.pos.x,
+(int)(g_game.cam.pos.y - g_game.cam.plane.y * lat_speed));
 	if (!map_value(
 	(int)(g_game.cam.pos.x - g_game.cam.plane.x * lat_speed),
 	(int)g_game.cam.pos.y))
@@ -44,8 +71,12 @@ void	left(double lat_speed)
 		g_game.cam.pos.y -= g_game.cam.plane.y * lat_speed;
 }
 
-void	right(double lat_speed)
+void		right(double lat_speed)
 {
+	check_is_monster((int)(g_game.cam.pos.x + g_game.cam.plane.x * lat_speed),
+			(int)g_game.cam.pos.y);
+	check_is_monster((int)g_game.cam.pos.x,
+			(int)(g_game.cam.pos.y + g_game.cam.plane.y * lat_speed));
 	if (!map_value(
 	(int)(g_game.cam.pos.x + g_game.cam.plane.x * lat_speed),
 	(int)g_game.cam.pos.y))
