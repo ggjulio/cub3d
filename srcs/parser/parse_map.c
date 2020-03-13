95bacf15ac0379677e498546c75d6ad69e9861bf
ggjulio <juligonz@student.42.fr>
2020-03-13 03:58:18 +0100

    .

--- srcs/parser/parse_map.c
+++ srcs/parser/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: juligonz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 19:09:50 by juligonz          #+#    #+#             */
-/*   Updated: 2020/03/13 03:07:40 by juligonz         ###   ########.fr       */
+/*   Updated: 2020/03/13 03:56:32 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,21 +53,21 @@ int		valid_map_closed_map(void)
 	while (++x < g_game.map_len_x)
 		if (!ft_in_charset((map_value(x, 0) + '0'), " 1") ||
 			!ft_in_charset((map_value(x, g_game.map_len_y - 1) + '0'), " 1"))
-			return (ft_error("Map : Maps no well closed"));
+			return (ft_error("Map : Map not well closed"));
 	y = 0;
 	while (++y < g_game.map_len_y - 1)
 	{
 		if (!ft_in_charset((map_value(0, y) + '0'), "1 ") ||
 			!ft_in_charset((map_value(g_game.map_len_x - 1, y) + '0'), "1 "))
-			return (ft_error("Map : Maps no well closed"));
+			return (ft_error("Map : Maps not well closed"));
 		x = 0;
 		while (++x < g_game.map_len_x - 1)
-			if (ft_in_charset((map_value(x, y) + '0'), "02NSWE"))
+			if (ft_in_charset((map_value(x, y) + '0'), "023NSWE"))
 				if ((char)map_value(x - 1, y) + '0' == ' ' ||
 					(char)map_value(x + 1, y) + '0' == ' ' ||
 					(char)map_value(x, y - 1) + '0' == ' ' ||
 					(char)map_value(x, y + 1) + '0' == ' ')
-					return (ft_error("Map : Maps no well closed"));
+					return (ft_error("Map : Maps not well closed"));
 	}
 	return (0);
 }
